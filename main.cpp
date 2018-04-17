b6b038cc47541d5f3b1e0bd057f28a21f5df6b7d
e-xxon <37479964+e-xxon@users.noreply.github.com>
2018-04-18 01:31:53 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,31 +3,22 @@
 using namespace std;
 
 class matrix {
-private:
-    int n;
-    int m;
-    int **matr;
+    private:
+    long int n;
+    long int m;
+    long int **matr;
     void crmatr() {
-        matr = new int*[n];
-        for(int i = 0; i < n; i++) {
-            matr[i] = new int[m];
-            for( int j = 0; j < m; j++)
-                matr[i][j] = 0;
-        }
-    }
-public:
+        matr = new long int*[m];
+            for (int i = 0; i < m; i++)
+                matr[i] = new long int[n];
+        	}
+    public:
     friend ostream& operator << (ostream& str, const matrix comp);
     friend istream& operator >> (istream& str, const matrix comp);
     //constructor
-    matrix(): m(5), n(5) {
-        crmatr();
-    }
-    matrix(long int i): m(i), n(i) {
-        crmatr();
-    }
-    matrix(long int i, long int j): m(i), n(j) {
-        crmatr();
-    }
+    matrix(): m(5), n(5) { crmatr(); }
+    matrix(long int i): m(i), n(i) { crmatr(); }
+    matrix(long int i, long int j): m(i), n(j) { crmatr(); }
     //destructor
     ~matrix() {
         for (int column = 0 ; column < m; column++)
@@ -35,22 +26,22 @@ public:
         delete[] matr;
     }
     void get() {
-        for(int i = 0; i < n; i++)
+        for(int i = 0;i < n; i++)
             for(int j = 0; j < m; j++)
                 cin >> matr[i][j];
     }
     matrix operator+ (matrix comp) {
         matrix result(comp.n, comp.m);
         if(this->n == comp.n && this->m == comp.m) {
-            for(int i = 0; i < comp.n; i++)
-                for(int j = 0; j < comp.m; j++)
-                    result.matr[i][j] = comp.matr[i][j] + this->matr[i][j];
+        for(int i = 0;i < comp.n; i++)
+            for(int j = 0; j < comp.m; j++)
+                result.matr[i][j] = comp.matr[i][j] + this->matr[i][j];
         }
         else throw;
     }
-    matrix operator+ (int a) {
+     matrix operator+ (int a) {
         matrix result(this->n,this->m);
-        for(int i = 0; i < this->n; i++)
+        for(int i = 0;i < this->n; i++)
             for(int j = 0; j < this->m; j++)
                 result.matr[i][j] = this->matr[i][j] + a;
         return result;
@@ -59,15 +50,15 @@ public:
     matrix operator- (matrix comp) {
         matrix result(comp.n,comp.m);
         if(this->n == comp.n && this->m == comp.m) {
-            for(int i = 0; i < comp.n; i++)
-                for(int j = 0; j < comp.m; j++)
-                    result.matr[i][j] = comp.matr[i][j] - this->matr[i][j];
+        for(int i = 0;i < comp.n; i++)
+            for(int j = 0; j < comp.m; j++)
+                result.matr[i][j] = comp.matr[i][j] - this->matr[i][j];
         }
         throw;
     }
     matrix operator-(int a) {
         matrix result(this->n,this->m);
-        for(int i = 0; i < this->n; i++)
+        for(int i = 0;i < this->n; i++)
             for(int j = 0; j < this->m; j++)
                 result.matr[i][j] = this->matr[i][j] - a;
         return result;
@@ -75,7 +66,7 @@ public:
 
     matrix operator*(int a) {
         matrix result(this->n,this->m);
-        for(int i = 0; i < this->n; i++)
+        for(int i = 0;i < this->n; i++)
             for(int j = 0; j < this->m; j++)
                 result.matr[i][j] = this->matr[i][j] * a;
     }
@@ -83,21 +74,21 @@ public:
 
     matrix& operator += (int a) {
 
-        for(int i = 0; i < this->n; i++)
+        for(int i = 0;i < this->n; i++)
             for(int j = 0; j < this->m; j++)
                 this->matr[i][j] = this->matr[i][j] + a;
         return *this;
     }
     matrix& operator -= (int a) {
 
-        for(int i = 0; i < this->n; i++)
+        for(int i = 0;i < this->n; i++)
             for(int j = 0; j < this->m; j++)
                 this->matr[i][j] = this->matr[i][j] - a;
         return *this;
     }
     matrix& operator *= (int a) {
 
-        for(int i = 0; i < this->n; i++)
+        for(int i = 0;i < this->n; i++)
             for(int j = 0; j < this->m; j++)
                 this->matr[i][j] = this->matr[i][j] * a;
         return *this;
@@ -106,18 +97,18 @@ public:
 
     matrix  operator = (matrix comp) {
         matrix result(comp.n,comp.m);
-        for(int i = 0; i < comp.n; i++)
+        for(int i = 0;i < comp.n; i++)
             for(int j = 0; j < comp.m; j++)
                 result.matr[i][j] =  comp.matr[i][j];
 
         for(int i = 0; i < this->n; i++) {
             delete(this->matr[i]);
-        }
-        delete(this->matr);
+          }
+          delete(this->matr);
         this->n = comp.n;
         this->m = comp.m;
         this->matr = result.matr;
-        return *this;
+            return *this;
     }
 
 
@@ -132,43 +123,40 @@ public:
 
     matrix trans() {
         matrix result(this->n,this->m);
-        for(int i = 0; i < this->n; i++)
+        for(int i = 0;i < this->n; i++)
             for(int j = 0; j < this->m; j++)
                 result.matr[i][j] = this->matr[j][i];
         return result;
     }
 
-    matrix atod (matrix comp) {
-        matrix result(this->n,comp.m);
+   matrix atod (matrix comp) {
+    matrix result(this->n,comp.m);
         if(this->m == comp.n) {
-            for(int i = 0; i < this->n; i++)
+            for(int i = 0;i < this->n; i++)
                 for(int j = 0; j < comp.m; j++)
                     for(int k = 0; k < comp.n ; k++) {
                         result.matr[i][j]+= this->matr[i][k] * comp.matr[k][j];
                     }
             return result;
         }
-        else throw;
-    }
-    void print(ostream& str) { //this is for the 162 line
-        for (int i = 0; i < this->n; i++) {
-            for (int j = 0; j < this->m; j++) {
-                str << this->matr[i][j] << " ";
-            }
-            str << endl;
-        }
+		else throw;
     }
 };
 
 istream& operator >> (istream& is, matrix comp) {
-    for(int i = 0; i < comp.n; i++)
+    for(int i = 0; i < comp.n;i++)
         for(int j = 0; j < comp.n; j++)
             is >> comp.matr[i][j];
 }
-ostream& operator << (ostream& str, matrix comp) { //for this:)
-    comp.print(str);
+ostream& operator << (ostream& str, matrix comp) {
+    for (int i = 0; i < comp.n; i++) {
+        for (int j = 0; j < comp.m; j++) {
+            str << comp.matr[i][j] << " ";
+        }
+    }
+    str << endl;
 }
+
 int main() {
     return 0;
 }
-
